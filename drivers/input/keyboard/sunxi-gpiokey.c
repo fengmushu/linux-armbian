96177fe99e4b3bd10e11849142dab4e1ce90d078
fengmushu <fengmushu@gmail.com>
2019-08-28 23:05:58 +0800

    sunxi gpio-key for poweroff

--- drivers/input/keyboard/sunxi-gpiokey.c
+++ drivers/input/keyboard/sunxi-gpiokey.c
@@ -12,12 +12,14 @@
 #include <linux/clk.h>
 #include <mach/sys_config.h>
 #include <linux/gpio.h>
+#include <linux/jiffies.h>
 
 static volatile u32 key_val;
-static struct gpio_config	power_key_io;
+static struct gpio_config	power_key_io, power_key_ctl;
 static struct mutex		gk_mutex;
 static struct input_dev *sunxigk_dev;
-static int suspend_flg = 0;
+static int suspend_flg = 0, powerkey_is_down = 0;
+static unsigned long poweroff_delay = 0;
 
 #ifdef CONFIG_PM
 static struct dev_pm_domain keyboard_pm_domain;
@@ -29,11 +31,74 @@ enum {
 	DEBUG_DATA_INFO = 1U << 2,
 	DEBUG_SUSPEND = 1U << 3,
 };
-static u32 debug_mask = 0;
+static u32 debug_mask = 255;
 #define dprintk(level_mask, fmt, arg...)	if (unlikely(debug_mask & level_mask)) \
 	printk(KERN_DEBUG fmt , ## arg)
 
 module_param_named(debug_mask, debug_mask, int, 0644);
+
+static u32 default_delay = (15 * HZ); /* MS to delay */
+module_param_named(default_delay, default_delay, int, 0644);
+
+static u32 force_off_delay = (5 * HZ); /* MS to delay force poweroff */
+module_param_named(force_off_delay, force_off_delay, int, 0644);
+
+static void fn_timer_poweroff(unsigned long);
+static DEFINE_TIMER(timer_poweroff, fn_timer_poweroff, 0, 0);
+
+static void do_poweroff(void)
+{
+	__gpio_set_value(power_key_ctl.gpio, 0);
+}
+
+static void fn_timer_poweroff(unsigned long data)
+{
+	/* keep pressing down > 5s */
+	if(powerkey_is_down && 
+		time_after(jiffies, powerkey_is_down + force_off_delay)) 
+	{
+		pr_info("Force sucide NOW...\n");
+		do_poweroff();
+	}
+	/* power-key released */
+	if(time_before(jiffies, poweroff_delay)) {
+		pr_info(".");
+		mod_timer(&timer_poweroff, jiffies + HZ);
+	} else {
+		pr_info("On timer sucide poweroff...\n");
+		do_poweroff();
+	}
+}
+
+static void trigger_poweroff_timer(int delay)
+{
+	poweroff_delay = jiffies + delay;
+	pr_info("start sucide timer[%d]...\n", delay);
+	mod_timer(&timer_poweroff, jiffies + HZ);
+}
+
+static int poweroff_set(const char *val, const struct kernel_param *kp)
+{
+	int n = 0, ret;
+
+	ret = kstrtoint(val, 10, &n);
+	if (ret != 0 || n < 1 || n > 120) {
+		pr_err("%s: err fmt: %s; <1-120>\n", __func__, val);
+		return -EINVAL;
+	}
+
+	trigger_poweroff_timer((n * HZ));
+
+	return param_set_int(val, kp);
+}
+
+static const struct kernel_param_ops param_ops = {
+	.set	= poweroff_set,
+	.get	= param_get_int,
+};
+
+static int poweroff_now;
+module_param_cb(poweroff, &param_ops, &poweroff_now, 0664);
 
 static irqreturn_t sunxi_isr_gk(int irq, void *dummy)
 {
@@ -49,11 +114,19 @@ static irqreturn_t sunxi_isr_gk(int irq, void *dummy)
 		input_report_key(sunxigk_dev, KEY_POWER, 0);
 		input_sync(sunxigk_dev);
 		dprintk(DEBUG_INT, "report data: power key up \n");
+		/* setup sucide timer */
+		if(powerkey_is_down) {
+			trigger_poweroff_timer(default_delay);
+		}
+		powerkey_is_down = 0;
 	}else{
 		input_report_key(sunxigk_dev, KEY_POWER, 1);
 		input_sync(sunxigk_dev);
 		dprintk(DEBUG_INT, "report data: power key down \n");
 		suspend_flg = 0;
+		if(!powerkey_is_down) {
+			powerkey_is_down = jiffies;
+		}
 	}
 	mutex_unlock(&gk_mutex);
 	return IRQ_HANDLED;
@@ -94,6 +167,14 @@ static int sunxigk_script_init(void)
 		power_key_io = val.gpio;
 		dprintk(DEBUG_INT,"%s power key io number = %d.\n", __func__, power_key_io.gpio);
 
+		/* get the power control GPIO: PL10(H3V1) */
+		type = script_get_item("gpio_power_key", "key_ctl", &val);
+		if(SCIRPT_ITEM_VALUE_TYPE_PIO != type){
+			pr_err("%s: get key io err! \n", __func__);
+			return -1;
+		}
+		power_key_ctl = val.gpio;
+		dprintk(DEBUG_INT,"%s power control io number = %d.\n", __func__, power_key_ctl.gpio);
 	}else{
 		dprintk(DEBUG_INT,"sunxi io power key no used.\n");
 		return -1;
@@ -168,7 +249,7 @@ fail1:
 
 static void __exit sunxigk_exit(void)
 {	
-
+	del_timer_sync(&timer_poweroff);
 	devm_free_irq(&sunxigk_dev->dev, gpio_to_irq(power_key_io.gpio), NULL);
 	input_unregister_device(sunxigk_dev);
 }
